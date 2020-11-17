442b01e2521c994373f4206a2712d7a80e2ecf08
Kateryna Batrakova <batrakovak@localadmins-MacBook-Pro.local>
2020-11-17 18:02:41 -0500

    Step 3 - Create and display Hero interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero'
 
 
 // @Component is a decorator function that specifies the Angular metadata for the component.
@@ -8,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 
+
+
 export class HeroesComponent implements OnInit {
-  //hero property
-  hero = 'Windstorm';
+  //hero property of imported type Hero (interface created)
+  hero: Hero = {
+    name: 'Windstorm', id: 1
+  };
 
 
+  constructor() { 
+    console.log(this.hero)
 
-  constructor() { }
+  }
 
 
 
