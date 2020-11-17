c37e36d412b5b732cdfd21b4fc878852223bfc78
Kateryna Batrakova <batrakovak@localadmins-MacBook-Pro.local>
2020-11-17 18:16:01 -0500

    Step 4 - Add two-way binding to the textbox, able to edit Hero name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+// import the FormsModule symbol
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  // @NgModule metadata's imports array contains external modules needed for an app
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
