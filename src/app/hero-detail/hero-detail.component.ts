d283c3000455e72df30391f19614146a9a6e9467
Kateryna Batrakova <batrakovak@localadmins-MacBook-Pro.local>
2020-11-19 18:29:46 -0500

    Step 9 - Create an injectable HeroService class with getHeroes method

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-@Component({
+  @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+// new service imports the Angular Injectable symbol
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+// class is annotated with the @Injectable() decorator providing metadata
+@Injectable({
+  // register a provider to make Service available to the DI system
+  // Provider instantiates the HeroService class to provide the service at "root" level
+  providedIn: 'root'
+})
+
+// HeroService class provides injectable service and can have own dependancies
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+}
