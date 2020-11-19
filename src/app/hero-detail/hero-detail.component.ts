edd37bdfbdca5d1b5820d4e0e4eb5dfdc963e4c5
Kateryna Batrakova <batrakovak@localadmins-MacBook-Pro.local>
2020-11-18 19:22:13 -0500

    Create a separate component for a selected hero, refactor parent HeroesComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+  // This component receives a hero object through its hero property and displays it
   // external HeroesComponent binds to it <app-hero-detail [hero]="selectedHero"></app-hero-detail>
   @Input() hero: Hero;
 
