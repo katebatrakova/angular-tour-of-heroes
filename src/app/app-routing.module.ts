93a5e3a93bfd1dd29123603b4bc2f0536d9a7baf
Kateryna Batrakova <batrakovak@localadmins-MacBook-Pro.local>
2020-11-20 09:17:35 -0500

    Step 12 - Add Dashboard component and its navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+];
+
+// metadata initializes the router and starts listening for browser location changes
 @NgModule({
+  // forRoot() method because you configure the router at the application's root level.
+  // forRoot() method supplies the service providers and directives needed for routing, and performs the initial navigation based on the current browser URL.
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  // inject the HeroService into a private heroService property
+  constructor(private heroService:HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5)); //returning only four of the Top Heroes
+  }
+}
